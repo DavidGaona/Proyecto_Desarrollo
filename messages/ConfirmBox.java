ad1f1a451d8cee60803f0ab44b67db9e38d4dfe0
Ikegno <reyes.606@hotmail.com>
2020-01-15 21:27:52 -0500

    ConfirmBox Style

--- messages/ConfirmBox.java
+++ messages/ConfirmBox.java
@@ -8,17 +8,21 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class ConfirmBox {
 
     static boolean answer;
 
     public static boolean display(String title, String message)
     {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Stage window = new Stage();
 
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
-        window.setMinWidth(250);
+        window.setMinWidth(screenSize.getWidth() * 0.30);
+        window.setMinHeight(screenSize.getHeight() * 0.25);
 
         Label label = new Label();
         label.setText(message);
@@ -26,8 +30,8 @@ public class ConfirmBox {
         //
         Button yesButton = new Button();
         Button noButton = new Button();
-        yesButton.setText("Salir");
-        noButton.setText("Cancelar");
+        yesButton.setText("Sí quiero salir del programa");
+        noButton.setText("No quiero salir del programa");
 
         yesButton.setOnAction( e -> {
             answer = true;
@@ -42,8 +46,11 @@ public class ConfirmBox {
         VBox layout = new VBox(10);
         layout.getChildren().addAll(label,yesButton,noButton);
         layout.setAlignment(Pos.CENTER);
+        layout.setStyle("-fx-background-color: #22282A");
         Scene scene = new Scene(layout);
+        scene.getStylesheets().add("Popup.css");
         window.setScene(scene);
+        window.setResizable(false);
         window.showAndWait();
 
         return answer;
