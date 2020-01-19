9a738ee9bc22257108a845d96cfe36651be4c47f
abcd452 <davidgaona2@gmail.com>
2020-01-18 23:00:53 -0500

    Removed useless declarations, made login main scene

--- Main.java
+++ Main.java
@@ -1,52 +1,38 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import utilities.ConfirmBox;
-import view.ClientMenu;
 import view.ClientMenuScene;
+import view.Login;
 
 import java.awt.*;
 
 
 public class Main extends Application {
 
-    private Dimension screenSize;
-    private double percentageWidth;
-    private double percentageHeight;
-    private double percentage;
-    private double buttonFont;
-    private GraphicsDevice gd;
-    private GraphicsConfiguration graphicsConfiguration;
-    private java.awt.Insets scnMax;
-    private int taskBarSize;
-    private int width;
-    private int height;
-
     @Override
     public void start(Stage window) {
         //resolutions
-        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        percentageWidth = (1920 - screenSize.getWidth()) / 1920;
-        percentageHeight = (1080 - screenSize.getHeight()) / 1080;
-        percentage = Math.max(percentageWidth, percentageHeight);
-        buttonFont = 22 - (22 * percentage);
-        gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        graphicsConfiguration = gd.getDefaultConfiguration();
-        scnMax = Toolkit.getDefaultToolkit().getScreenInsets(graphicsConfiguration);
-        taskBarSize = scnMax.bottom;
-        width = (int) screenSize.getWidth(); //2560 1920 1280 1152 1024; 768 40
-        height = (int) screenSize.getHeight();//1440 1080 720 648 576; 432 40
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double percentageWidth = (1920 - screenSize.getWidth()) / 1920;
+        double percentageHeight = (1080 - screenSize.getHeight()) / 1080;
+        double percentage = Math.max(percentageWidth, percentageHeight);
+        double buttonFont = 22 - (22 * percentage);
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        GraphicsConfiguration graphicsConfiguration = gd.getDefaultConfiguration();
+        java.awt.Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(graphicsConfiguration);
+        double width = screenSize.getWidth();  //2560 1920 1280 1152 1024; 768 40
+        double height = screenSize.getHeight() - scnMax.bottom * 1.7;//1440 1080 720 648 576; 432 40
 
-        ClientMenuScene mainMenuClient = new ClientMenuScene(percentage,buttonFont);
+        ClientMenuScene mainMenuClient = new ClientMenuScene(percentage, buttonFont);
+        Login login = new Login(percentage, buttonFont);
 
         window.setOnCloseRequest(e -> {
             e.consume();
             closeProgram(window);
         });
-        window.setScene(mainMenuClient.renderScene(width,height,taskBarSize));
+
+        //window.setScene(mainMenuClient.renderScene(width, height));
+        window.setScene(login.renderLoginScene(width, height)); //1.8 si no te queda
         window.setTitle("Mobile plans solution");
         window.setResizable(false);
         window.show();
