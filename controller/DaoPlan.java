5f4973ca521ff6e5413ba30f8e8805cfe824c8cb
Al3xCodeMaster <gonzalez.alexander@correounivalle.edu.co>
2020-03-20 01:28:42 -0500

    Created Dao and model for plan

---
+++ controller/DaoPlan.java
@@ -0,0 +1,21 @@
+package controller;
+
+import connection.DbManager;
+import model.Plan;
+import utilities.ProjectUtilities;
+
+public class DaoPlan {
+
+    private DbManager dbManager = new DbManager("postgres", "postgres", "MobilePlan", "localhost");
+
+    public int saveNewPlan(String planName, double planCost, int planMinutes, int planData, int planTextMsn){
+        Plan plan = new Plan(planName,planCost,planMinutes, planData,planTextMsn);
+        int success = -1;
+        if (plan.isNotBlank()) {
+            dbManager.openDBConnection();
+            success = dbManager.saveNewPlan(plan);
+            dbManager.closeDBConnection();
+        }
+        return success;
+    }
+}
