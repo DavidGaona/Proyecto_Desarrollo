4d14b8286ce27dd947424744b77294f269701c74
camiloGonzalez96 <juan.berrio@correounivalle.edu.co>
2020-03-31 21:02:12 -0500

    Testing implementation

--- test/TestDaoUser.java
+++ test/TestDaoUser.java
@@ -8,16 +8,50 @@ public class TestDaoUser extends TestCase {
     public void scenario(){
         user = new DaoUser();
     }
-    //**************************** testsaveNewUser********************
-    public void testsaveNewUser(){ //caso exitoso
+    //****************************testsaveNewUser********************
+
+    public void testSaveNewUser(){ //caso exitoso
         scenario();
         assertEquals("Operacion exitosa",(user.saveNewUser(168,"pipe","malo","451",(short)0,(short)0,true,35)));
     }
 
-    public void testsaveNewUserCreado(){
+    public void testSaveNewUserCreado(){ //usuario existente
         scenario();
-        assertEquals("El usuario ya se encuentra creado",(user.saveNewUser(162,"pipe","malo","435",(short)0,(short)0,true,35)));
+        assertEquals("El usuario ya se encuentra creado",(user.saveNewUser(162,"pipe","malo","436",(short)0,(short)0,true,35)));
     }
+
+    //****************************editUser********************
+    public void testEditUser(){ //editar exitoso
+        scenario();
+        assertEquals("Usuario editado con exito",(user.editUser(162,"pipe","malo","435",(short)0,(short)0,true,false)));
+    }
+
+    //documento existente toca hacerlo manual
+
+    //****************************loadUser********************
+    public void testLoadUser(){ //carga correcta
+        scenario();
+        assertTrue(169==user.loadUser("436","Cédula de ciudadanía").getId());
+    }
+
+    //****************************loginUser********************
+    public void testLoginUser(){ //login correcto
+        scenario();
+        assertEquals(3 , user.loginUser("436", "436"));
+    }
+
+    public void testLoginUserIncorrecto(){ //password incorrecto
+        scenario();
+        assertFalse(3 == user.loginUser("436", "432346"));
+    }
+
+    public void testCheckPassword(){ //check correcto
+        scenario();
+        assertEquals(true , user.checkPassword(169,"436"));
+    }
+
+
+
 
 
 }
