482f94ba3b13bf53760057e187f9a83a0cfcdf21
camiloGonzalez96 <juan.berrio@correounivalle.edu.co>
2020-05-09 14:21:39 -0500

    Testing implementation, bill and plan addition

--- test/testDaoBank.java
+++ test/testDaoBank.java
@@ -10,25 +10,27 @@ public class testDaoBank extends TestCase {
         bank = new DaoBank();
     }
 
-  /*public void testSaveNewBank(){
+  /*
+  public void testSaveNewBank(){
         scenario();
         assertEquals("Operación Realizada",(bank.saveNewBank("Caja Social","157486","458451454")));
-    } */
+    }
+    */
     @Test
     public void testEditBank(){
         scenario();
-        assertTrue(bank.editBank(false,"2").equals("Operación Realizada"));
+        assertTrue(bank.editBank(true,"420").equals("Operación Realizada"));
     }
 
     @Test
     public void testLoadBank(){
         scenario();
-        assertEquals("333",bank.loadBank("banco3").getNIT());
+        assertEquals("69",bank.loadBank("Banco Agrario").getNIT());
     }
 
     @Test
     public void testLoadAllBanks(){          /**el numero esperado depende del numero de bancos que hay en la BD**/
         scenario();
-        assertEquals(7,bank.loadAllBanks().length);
+        assertEquals(3,bank.loadAllBanks().length);
     }
 }
