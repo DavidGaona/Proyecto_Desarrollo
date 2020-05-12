eeb3546a7cd4dadbd02f8c019a514dd1b931d640
Ikegno <reyes.606@hotmail.com>
2020-05-12 18:21:21 -0500

    monthsInSpanish, ProjectUtilities

--- utilities/ProjectUtilities.java
+++ utilities/ProjectUtilities.java
@@ -197,6 +197,38 @@ public class ProjectUtilities {
         }
     }
 
+    public static String monthsInSpanish(String month){
+        String lowerMonth = month.toLowerCase();
+        switch(lowerMonth) {
+            case "january":
+                return "Enero";
+            case "february":
+                return "Febrero";
+            case "march":
+                return "Marzo";
+            case "april":
+                return "Abril";
+            case "may":
+                return "Mayo";
+            case "june":
+                return "Junio";
+            case "july":
+                return "Julio";
+            case "august":
+                return "Agosto";
+            case "september":
+                return "Septiembre";
+            case "october":
+                return "Octubre";
+            case "november":
+                return "Noviembre";
+            case "december":
+                return "Diciembre";
+            default:
+                return "00";
+        }
+    }
+
     private static Node selectedNode, lastSelectedNode;
 
     public static void focusListener(GridPane layout, Node... nodes) {
